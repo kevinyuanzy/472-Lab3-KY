1c2cc3557515c2527b745a9b4e2ad9a45de920b4
kevinyuanzy <yuansirzy126@gmail.com>
2025-03-01 09:39:06 -0500

    11

--- script.js
+++ script.js
@@ -5,22 +5,39 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW55dWFuenkiLCJhIjoiY201eHprYXU0MGZwejJsb
 const map = new mapboxgl.Map({
     container: 'Lab2Map', // map container ID in the index.html file.
     style: 'mapbox://styles/kevinyuanzy/cm6ztbpqc003s01qwcvdrf6ft', // style URL from created MapBox style.
-    center: [-79.391820, 43.671268], // starting position [lng, lat]. 
+    center: [-79.391820, 43.701268], // starting position [lng, lat]. 
     zoom: 11, // starting zoom level.
 });
 
 //Use "map.on" event listener to add features to the webmap.
 map.on('load', () => {
     //Add the station_points.geojson file from GitHub repository. 
-    map.addSource('stations_points', {
+    map.addSource('stations_completed', {
         type: 'geojson',
-        data: 'https://raw.githubusercontent.com/kevinyuanzy/472-Lab3-KY/refs/heads/main/stations_points.geojson' // The URL to my GeoJson polygon.
+        data: 'https://raw.githubusercontent.com/kevinyuanzy/472-Lab3-KY/refs/heads/main/stations_completed.geojson' // The URL to my GeoJson polygon.
     });
 
     map.addLayer({
-        'id': 'line2-stations', 
+        'id': 'line2-completed-stations', 
         'type': 'circle', 
-        'source': 'stations_points',
+        'source': 'stations_completed',
+        'paint': {
+            'circle-color': '#f5f5f5',
+            'circle-radius': 5,
+            'circle-stroke-width': 1.5,
+            'circle-stroke-color': '#000000'
+        },
+    });
+
+    map.addSource('stations_incompleted', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/kevinyuanzy/472-Lab3-KY/refs/heads/main/stations_incompleted.geojson' // The URL to my GeoJson polygon.
+    });
+
+    map.addLayer({
+        'id': 'line2-incompleted-stations', 
+        'type': 'circle', 
+        'source': 'stations_incompleted',
         'paint': {
             'circle-color': '#f5f5f5',
             'circle-radius': 5,
